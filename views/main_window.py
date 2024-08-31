a8ea7b7c69fcebc9556d58c7d3027d6ebe01be6e
Sumukh Prasad <sumukhprasad@icloud.com>
2024-08-31 18:58:42 +0530

    Added clickable maps.

--- views/main_window.py
+++ views/main_window.py
@@ -1,19 +1,19 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QListWidget, QListWidgetItem, QLineEdit, QFormLayout, QDialog, QDialogButtonBox, QMessageBox, QHBoxLayout, QLabel
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtGui import QIcon
-import folium
-import os
 import tempfile
+import folium
+from PyQt5.QtCore import Qt, QUrl, pyqtSlot, QObject
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QListWidget, QListWidgetItem, QLineEdit, QFormLayout, QDialog, QDialogButtonBox, QMessageBox, QHBoxLayout, QLabel
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebChannel import QWebChannel
 
 class NoteDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Add Note")
 
-        # Create map
+        # Create map view
         self.map_view = QWebEngineView()
         self.map_file = self.create_map_html()
+        self.map_view.setUrl(QUrl.fromLocalFile(self.map_file))
 
         # Create input fields
         self.note_edit = QLineEdit()
@@ -37,13 +37,23 @@ class NoteDialog(QDialog):
 
         self.setLayout(layout)
 
-        # Load the map
-        self.map_view.setUrl(QUrl.fromLocalFile(self.map_file))
-
         # Set default values for latitude and longitude
         self.lat_edit.setText("51.5074")  # Default latitude (e.g., London)
         self.lon_edit.setText("-0.1278")  # Default longitude (e.g., London)
-        
+
+        # Set up JavaScript interface
+        self.web_channel = QWebChannel()
+        self.js_interface = JavaScriptInterface(self)
+        self.web_channel.registerObject("python", self.js_interface)
+        self.map_view.page().setWebChannel(self.web_channel)
+
+        # Initialize JavaScript after page load
+        self.map_view.loadFinished.connect(self.on_load_finished)
+
+    def on_load_finished(self, ok):
+        if ok:
+            self.map_view.page().runJavaScript('initializeMap();')
+
     def create_map_html(self):
         # Create a temporary file for the map
         temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.html')
@@ -54,7 +64,35 @@ class NoteDialog(QDialog):
         folium_map = folium.Map(location=map_center, zoom_start=13)
 
         # Add a marker to the map
-        folium.Marker(location=map_center, popup="You are here").add_to(folium_map)
+        folium.Marker(location=map_center, popup="You are here", draggable=True).add_to(folium_map)
+
+        # Add JavaScript for handling clicks
+        folium_map.get_root().html.add_child(folium.Element('''
+	   <script type="text/javascript" src="qrc:///qtwebchannel/qwebchannel.js"></script>
+        <script>
+	   var python = null;
+	   new QWebChannel(qt.webChannelTransport, function(channel) {
+		   python = channel.objects.python;
+	   });
+        function initializeMap() {
+            var map = window[document.querySelector('.leaflet-container').id];
+            if (map) {
+                map.on('click', function(e) {
+                    var lat = e.latlng.lat;
+                    var lng = e.latlng.lng;
+                    console.log('Clicked coordinates:', lat, lng);
+                    if (window.python) {
+                        python.handleMapClick(lat, lng);
+                    } else {
+                        console.error('Python interface not available');
+                    }
+                });
+            } else {
+                console.error('Map is not initialized');
+            }
+        }
+        </script>
+        '''))
 
         # Save the map to the temp HTML file
         folium_map.save(temp_file.name)
@@ -67,6 +105,17 @@ class NoteDialog(QDialog):
             'latitude': self.lat_edit.text(),
             'longitude': self.lon_edit.text()
         }
+
+class JavaScriptInterface(QObject):
+    def __init__(self, dialog):
+        super().__init__()
+        self.dialog = dialog
+
+    @pyqtSlot(float, float)
+    def handleMapClick(self, lat, lng):
+        self.dialog.lat_edit.setText(str(lat))
+        self.dialog.lon_edit.setText(str(lng))
+
 
 class NoteItemWidget(QWidget):
     def __init__(self, note_text, delete_callback, parent=None):
